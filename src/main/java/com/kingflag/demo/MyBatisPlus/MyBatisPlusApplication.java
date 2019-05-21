cf208337393aa0f86583ee7253e305f4d62a0105
kingflag.wang <kingflag.wang@cardinfolink.com>
2019-05-21 17:11:22 +0800

    init

---
+++ src/main/java/com/kingflag/demo/MyBatisPlus/MyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.kingflag.demo.MyBatisPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBatisPlusApplication.class, args);
+	}
+
+}
